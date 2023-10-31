ab81da9e778c2ec350bc68c80d665be9d1588115
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-10-31 10:01:46 +0100

    Created data patch for product attribute. 	new file:   Attributes.php 	modified:   Setup/Patch/Data/CreateSkuOverrideAttribute.php 	new file:   etc/catalog_attributes.xml

---
+++ Attributes.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Denal05\SwiftOtterOrderExport;
+
+class Attributes
+{
+    const SKU_OVERRIDE_ATTRIBUTE = 'sku_override';
+}
--- Setup/Patch/Data/CreateSkuOverrideAttribute.php
+++ Setup/Patch/Data/CreateSkuOverrideAttribute.php
@@ -7,6 +7,9 @@ declare(strict_types=1);
 
 namespace Denal05\SwiftOtterOrderExport\Setup\Patch\Data;
 
+use Denal05\SwiftOtterOrderExport\Attributes;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
 
@@ -21,11 +24,19 @@ class CreateSkuOverrideAttribute implements DataPatchInterface
     private $moduleDataSetup;
 
     /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
      * @param ModuleDataSetupInterface $moduleDataSetup
      */
-    public function __construct(ModuleDataSetupInterface $moduleDataSetup)
-    {
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory,
+    ) {
         $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
     }
 
     /**
@@ -35,6 +46,36 @@ class CreateSkuOverrideAttribute implements DataPatchInterface
      */
     public function apply()
     {
+        /** @var \Magento\Eav\Setup\EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            Attributes::SKU_OVERRIDE_ATTRIBUTE,
+            [
+                'wysiwyg_enabled' => false,
+                'html_allowed_on_front' => false,
+                'used_for_sort_by' => false,
+                'filterable' => false,
+                'filterable_in_search' => false,
+                'used_in_grid' => false,
+                'visible_in_grid' => false,
+                'filterable_in_grid' => false,
+                'position' => 0,
+                'apply_to' => 'simple,virtual',
+                'searchable' => false,
+                'visible_in_advanced_search' => false,
+                'comparable' => false,
+                'used_for_promo_rules' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'visible' => false,
+                'scope' => 'global',
+                'input' => 'text',
+                'required' => false,
+                'label' => 'SKU Override',
+                'type' => 'varchar',
+            ]
+        );
     }
 
     /**
