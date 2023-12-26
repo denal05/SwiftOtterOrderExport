fb8f0878eca03fe03c725e24d2d6b810bdf182a0
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2023-12-26 12:42:40 +0100

    Created an Orchestrator class and added its object to Controller\Adminhtml\Export\Run::execute() 	modified:   Controller/Adminhtml/Export/Run.php 	new file:   Orchestrator.php

--- Controller/Adminhtml/Export/Run.php
+++ Controller/Adminhtml/Export/Run.php
@@ -5,25 +5,30 @@ namespace Denal05\SwiftOtterOrderExport\Controller\Adminhtml\Export;
 
 use Denal05\SwiftOtterOrderExport\Model\HeaderData;
 use Denal05\SwiftOtterOrderExport\Model\HeaderDataFactory;
+use Denal05\SwiftOtterOrderExport\Orchestrator;
 use Magento\Backend\App\Action as AdminAction;
 use Magento\Backend\App\Action\Context as ActionContext;
 use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class Run extends AdminAction implements HttpPostActionInterface
 {
     private JsonFactory $jsonFactory;
     private HeaderDataFactory $headerDataFactory;
+    private Orchestrator $orchestrator;
 
     public function __construct(
         HeaderDataFactory $headerData,
+        Orchestrator $orchestrator,
         JsonFactory $jsonFactory,
         ActionContext $context,
     ) {
         parent::__construct($context);
         $this->jsonFactory = $jsonFactory;
         $this->headerDataFactory = $headerData;
+        $this->orchestrator = $orchestrator;
     }
 
     public function execute()
@@ -39,6 +44,15 @@ class Run extends AdminAction implements HttpPostActionInterface
                 $this->getRequest()->getParam(HeaderData::MERCHANT_NOTES)
             )
         );
+
+        try {
+            $this->orchestrator->run(
+                $this->getRequest()->getParam('order_id'),
+                $headerData
+            );
+        } catch(NoSuchEntityException $exception) {
+
+        }
 
         return $this->jsonFactory->create();
     }
---
+++ Orchestrator.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Denal05\SwiftOtterOrderExport;
+
+use Denal05\SwiftOtterOrderExport\Action\TransformOrderToArray;
+use Denal05\SwiftOtterOrderExport\Model\HeaderData;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+class Orchestrator
+{
+    private TransformOrderToArray $orderToArray;
+
+    public function __construct(
+        TransformOrderToArray $orderToArray
+    ) {
+
+        $this->orderToArray = $orderToArray;
+    }
+
+    public function run(
+        int $orderId,
+        HeaderData $headerData
+    ): array {
+        // throw new NoSuchEntityException(__('There was an error: no such entity'));
+
+        $results = [
+            'success' => false,
+            'error' => null
+        ];
+
+        $orderDetails = $this->orderToArray->execute($orderId, $headerData);
+
+        return $results;
+    }
+}
